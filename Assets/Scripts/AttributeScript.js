e16c50dc75be72256370cb314bbcba6e0bed5d0e
vanguardofnight <vanguardofnight@aol.com>
2014-04-07 16:00:46 -0500

    Commit_2 Falling blocks/etc

---
+++ Assets/Scripts/AttributeScript.js
@@ -0,0 +1,76 @@
+﻿#pragma strict
+
+var strength : int = 1;
+var weight : int = 1;
+var water : int = 10; //player's water level, will also be used to size water bar
+var framecount = 0; //used to only decrement water in specific intervals
+var difficulty = 30;
+/*difficulty is a parameter that is inversely proportional to the 
+speed at which the water depletes. For example: a difficulty setting of 
+120 means that the water level for the player will decrease every 2 seconds, but a 
+difficulty level of 60 means the water level will deplete every 1 second 
+*/
+
+/*debugging purpose, remove later*/
+var ScoreTextPosition : Rect = Rect(0, 0, 256, 128);
+var style : GUIStyle;
+var ScoreText = "water:  ";
+
+
+function Start () {
+	//for debugging, remove this line later
+	style.normal.textColor = Color.white;
+}
+
+function incrementWeight(){
+	weight = weight + 1;
+}
+
+function incrementStrength(){
+	strength = strength + 1;
+}
+
+function getWeight(){
+	return weight;
+}
+
+function getStrength(){
+	return strength;
+}
+
+function refillWater(){
+	water = 10;
+}
+
+function getWater(){
+	return water;
+}
+
+function depleteWater(){
+		Debug.Log(water);
+		water = getWater()- 1;
+		if(water <= 0){
+			Application.LoadLevel("gameover");
+		}
+	
+}
+/*debugging purpose, remove this function later */
+function OnGUI()
+{
+	GUI.Label(ScoreTextPosition, ScoreText, style);
+}
+
+
+function Update () {
+	/*decrement water level at a constant rate during gameplay */
+		if(framecount == difficulty){
+		depleteWater();
+		framecount = 0;
+	} else {
+		framecount = framecount + 1;
+		}
+		ScoreText = "water level: " + water + "\nstrength: "+ strength + 
+		"\nweight = " + weight;
+		//debugging purposes, remove later
+		GUI.Label(ScoreTextPosition, ScoreText, style);
+}
