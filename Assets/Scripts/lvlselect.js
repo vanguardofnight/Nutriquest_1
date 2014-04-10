aa01fa087d76606dd7d64f03f14f23a94af82224
Jinsoo Kim <kim10987@dhcp-10-202-148-141.tamulink.tamu.edu>
2014-04-10 14:58:55 -0500

    Merge branch 'Jinsoo'
    Conflicts:
    Library/CurrentLayout.dwlt
    Library/assetDatabase3

---
+++ Assets/Scripts/lvlselect.js
@@ -0,0 +1,22 @@
+﻿function Start() 
+{
+	Screen.autorotateToLandscapeRight = true;
+}
+
+function OnMouseDown(){
+	if(gameObject.name == "Level1_Texture") {
+		Application.LoadLevel("lvl1");
+	}	
+	if(gameObject.name == "Level2_Texture") {
+		Application.LoadLevel("lvl2");
+	}
+	if(gameObject.name == "Level3_Texture") {
+		Application.LoadLevel("lvl3");
+	}
+	if(gameObject.name == "Level4_Texture") {
+		Application.LoadLevel("lvl4");
+	}
+	if(gameObject.name == "Mainmenu_Texture") {
+		Application.LoadLevel("intro");
+	}
+}
