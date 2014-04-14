3559c73b195313eabe1d68c9ccfb135c8619b1eb
Jinsoo Kim <kim10987@Test.local>
2014-04-14 16:12:45 -0500

    Lv4 random food generator, fixed bugs
    Lv4 random food generator (objects w/ difff timers)
    Fixed button response problem
    
    Known issue:
    3D collider : z-axis problem
    Joystick: GUI Position problem.
    
    Suggestion:
    Switch 3D from 3D.
    Jinsoo will re-work 2D Side Scroller and Controller just for Scene 4
    (Do not modify Scene 4)

---
+++ Assets/Scripts/Lv4Attribute.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+
+private var PlayerScore = 0;
+
+function Start () {
+}
+
+function OnGUI() {
+	GUI.Label(Rect(0, 0, 256, 128), "Score: " + PlayerScore);
+}
+
+function Update () {
+}
+
+function OnTriggerEnter(other : Collider)
+{
+	if(other.tag == "Water")
+	{
+		Destroy(other.gameObject);
+		PlayerScore += 1;
+	}
+	if(other.tag == "JunkFood")
+	{
+		Destroy(other.gameObject);
+		PlayerScore -= 1;
+	}
+	
+	if(other.tag == "Dairy"){
+		Destroy(other.gameObject);
+		PlayerScore += 1;
+	}
+	
+	if( other.tag == "Fruit"){
+		Destroy(other.gameObject);
+		PlayerScore += 1;
+	}
+	
+	if( other.tag == "Grain"){
+		Destroy(other.gameObject);
+		PlayerScore += 1;
+	}
+	
+	if( other.tag == "Protein"){
+		Destroy(other.gameObject);
+		PlayerScore += 1;
+	}
+}
--- Assets/Scripts/mainmenu.js
+++ Assets/Scripts/mainmenu.js
@@ -1,16 +1,17 @@
 ﻿function Start() 
 {
-	Screen.autorotateToLandscapeRight = true;
+//	Screen.autorotateToLandscapeRight = true;
+ 	Screen.orientation = ScreenOrientation.LandscapeLeft;
 }
 
 function OnMouseDown(){
-	if(gameObject.name == "Newgame_Texture") {
+	if(gameObject.name == "Newgame_Texture" || gameObject.name == "Newgame_Txt") {
 		Application.LoadLevel("lvl1");
 	}	
-	if(gameObject.name == "Levelselect_Texture") {
+	if(gameObject.name == "Levelselect_Texture" || gameObject.name == "Levelselect_Txt") {
 		Application.LoadLevel("lvlselect");
 	}
-	if(gameObject.name == "Options_Texture") {
+	if(gameObject.name == "Options_Texture" || gameObject.name == "Options_Txt") {
 		Application.LoadLevel("settingsmenu");
 	}
 }
