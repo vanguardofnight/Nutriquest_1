90f00a9990fe1e2e136ae4562dad7d851131580c
Jinsoo Kim <kim10987@dhcp-10-201-148-159.tamulink.tamu.edu>
2014-04-28 08:53:39 -0500

    Replacing previous files by local copy

--- Assets/01_Scripts/ChangeNumber.js
+++
@@ -1,9 +0,0 @@
-﻿function ChangeNumber ( num : int) {
-	var sprite;
-	if (num != 10) {
-		sprite = Resources.Load("hud_" + num, Sprite);
-	} else {
-		sprite = Resources.Load("hud_x", Sprite);
-	}
-	GetComponent(SpriteRenderer).sprite = sprite;
-}
--- Assets/01_Scripts/WaterGauge.js
+++
@@ -1,9 +0,0 @@
-﻿private var defaultScale = 1;
-
-function Start () {
-	defaultScale = transform.localScale.x;
-}
-
-function Resize ( waterlevel: double, maxwater: double ) {
-	transform.localScale.x = waterlevel * defaultScale / maxwater;
-}
